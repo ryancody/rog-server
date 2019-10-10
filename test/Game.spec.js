0ca59cc7a16c8bec2daf06d6d677c19976d69787
Ryan Cody <ryan.p.cody@gmail.com>
2019-10-10 12:44:19 -0400

    finished Game class test spec

--- test/Game.spec.js
+++ test/Game.spec.js
@@ -3,11 +3,22 @@ let Game = require('../components/Game')
 
 describe('test Game class', function () {
 
+    let date = new Date().getTime()
     let id = 'tester'
     let game = new Game(id)
 
     it('Creates a new game and sets owner to passed ID', function () {
         
         assert.equal(game.owner, id)
+    })
+
+    it('sets property created to current epoch', function () {
+
+        assert.equal(game.created, date)
+    })
+
+    it('sets state to empty object', function () {
+
+        assert.equal(JSON.stringify(game.state), JSON.stringify({}))
     })
 })
